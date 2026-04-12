f8309edde3fafb524af821b92943bc1b416eb89a
Rooming1021 <tomato.dis@gmail.com>
2026-04-13 01:27:29 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+﻿window.CHAT_CONFIG = {
+  supabaseUrl: "https://xolvhdywflssvjyhgrsd.supabase.co",
+  supabasePublishableKey: "sb_publishable_-_4UzrSTbjOpGHnODG8JJA_jvUF_j9X",
+  roomSlug: "class-3-1-passion-on",
+  roomLabel: "3-1반 열정 ON!",
+  pollIntervalMs: 4000
+};
+
+
