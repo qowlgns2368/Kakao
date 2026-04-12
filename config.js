5a4f55dcf4d3662c7f7e8d55a104eea7f1d41d5e
Rooming1021 <tomato.dis@gmail.com>
2026-04-13 01:40:31 +0900

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-﻿window.CHAT_CONFIG = {
-  supabaseUrl: "https://xolvhdywflssvjyhgrsd.supabase.co",
-  supabasePublishableKey: "sb_publishable_-_4UzrSTbjOpGHnODG8JJA_jvUF_j9X",
-  roomSlug: "class-3-1-passion-on",
-  roomLabel: "3-1반 열정 ON!",
-  pollIntervalMs: 4000
-};
-
-
